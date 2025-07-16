96ec2c6300979ea999521eaa8f079e4576646675
Vipul Tiwari <tiwarivipul2018@gmail.com>
2025-07-16 23:13:51 +0530

    .env file added

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,8 +2,9 @@ import axios from "axios";
 import React from "react";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL + "/api/",
 });
+
 
 
 export const UserSignUp = async (data) => API.post("/user/signup", data);
